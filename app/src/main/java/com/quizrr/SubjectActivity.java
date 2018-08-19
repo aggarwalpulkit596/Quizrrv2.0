a43c898fed528f6cee7648802f05e36aac921085
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2018-08-19 22:29:03 +0530

    Collapable toolbar done

--- app/src/main/java/com/quizrr/SubjectActivity.java
+++ app/src/main/java/com/quizrr/SubjectActivity.java
@@ -1,15 +1,13 @@
 package com.quizrr;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,7 +17,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class SubjectActivity extends AppCompatActivity {
+public class SubjectActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
+
 
 //    @BindView(R.id.backButton)
 //    ImageButton backButton;
@@ -33,39 +32,53 @@ public class SubjectActivity extends AppCompatActivity {
 //    TextView chaptersTextView;
 //    @BindView(R.id.quizzesTextView)
 //    TextView quizzesTextView;
-    @BindView(R.id.accuracytextView)
-    TextView accuracytextView;
-    @BindView(R.id.accuracyPercentagetextView)
-    TextView accuracyPercentagetextView;
-    @BindView(R.id.quiztextView)
-    TextView quiztextView;
-    @BindView(R.id.quizPercentagetextView)
-    TextView quizPercentagetextView;
-    @BindView(R.id.ranktextView)
-    TextView ranktextView;
-    @BindView(R.id.rankPercentagetextView)
-    TextView rankPercentagetextView;
-    @BindView(R.id.recommendationCardTitle)
-    TextView continueCardTitle;
-    @BindView(R.id.chaptersRecyclerViewTitle)
-    TextView chaptersRecyclerViewTitle;
-    @BindView(R.id.chaptersRecyclerView)
-    RecyclerView chaptersRecyclerView;
-    @BindView(R.id.continueCardLayout)
-    CardView continueCardLayout;
+//    @BindView(R.id.accuracytextView)
+//    TextView accuracytextView;
+//    @BindView(R.id.accuracyPercentagetextView)
+//    TextView accuracyPercentagetextView;
+//    @BindView(R.id.quiztextView)
+//    TextView quiztextView;
+//    @BindView(R.id.quizPercentagetextView)
+//    TextView quizPercentagetextView;
+//    @BindView(R.id.ranktextView)
+//    TextView ranktextView;
+//    @BindView(R.id.rankPercentagetextView)
+//    TextView rankPercentagetextView;
+//    @BindView(R.id.recommendationCardTitle)
+//    TextView continueCardTitle;
+//    @BindView(R.id.chaptersRecyclerViewTitle)
+//    TextView chaptersRecyclerViewTitle;
+//    @BindView(R.id.chaptersRecyclerView)
+//    RecyclerView chaptersRecyclerView;
+//    @BindView(R.id.continueCardLayout)
+//    CardView continueCardLayout;
 
     String subjectId;
     private ArrayList<Chapter> chapterList = new ArrayList<>();
     ChapterAdapter chapterAdapter;
 
+    private static final int PERCENTAGE_TO_SHOW_IMAGE = 50;
+    private View mFab;
+    private int mMaxScrollSize;
+    @BindView(R.id.toolbar_layout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_subject);
+        setContentView(R.layout.app_bar_subject);
         ButterKnife.bind(this);
         Intent i = getIntent();
         subjectId = i.getStringExtra("subjectId");
+        GradientDrawable gradientDrawable;
+        if (0 == 0) {
+            gradientDrawable = new GradientDrawable(
+                    GradientDrawable.Orientation.BOTTOM_TOP, //set a gradient direction
+                    new int[]{0xFFffa885, 0xFFff758c}); //set the color of gradient
+            gradientDrawable.setCornerRadius(6f); //set corner radius
+            collapsingToolbarLayout.setBackground(gradientDrawable);
+        }
         init();
     }
 
@@ -73,10 +86,15 @@ public class SubjectActivity extends AppCompatActivity {
     private void init() {
         Intent i = getIntent();
         subjectId = i.getStringExtra("subjectId");
+        mFab = findViewById(R.id.aboutBar);
+
         chapterAdapter = new ChapterAdapter(chapterList, getApplicationContext());
-        chaptersRecyclerView.setHasFixedSize(true);
-        chaptersRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        chaptersRecyclerView.setAdapter(chapterAdapter);
+        AppBarLayout appbar = findViewById(R.id.app_bar);
+
+        appbar.addOnOffsetChangedListener(this);
+//        chaptersRecyclerView.setHasFixedSize(true);
+//        chaptersRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+//        chaptersRecyclerView.setAdapter(chapterAdapter);
         getSubject();
 
     }
@@ -90,10 +108,10 @@ public class SubjectActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     Subject subject = response.body();
                     chapterList.addAll(subject.getChapters());
-                    chaptersRecyclerView.smoothScrollToPosition(0);
+//                    chaptersRecyclerView.smoothScrollToPosition(0);
                     chapterAdapter.notifyDataSetChanged();
                     if (chapterList.size() == 0) {
-                        chaptersRecyclerViewTitle.setVisibility(View.GONE);
+//                        chaptersRecyclerViewTitle.setVisibility(View.GONE);
                     }
 //                    titleTextView.setText(subject.getName());
 //                    chaptersTextView.setText(subject.getChapterCount()+" Chapters");
@@ -109,4 +127,30 @@ public class SubjectActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+        if (mMaxScrollSize == 0)
+            mMaxScrollSize = appBarLayout.getTotalScrollRange();
+
+        int currentScrollPercentage = (Math.abs(i)) * 100
+                / mMaxScrollSize;
+
+        if (currentScrollPercentage >= PERCENTAGE_TO_SHOW_IMAGE) {
+            ViewCompat.animate(mFab).scaleY(0).scaleX(0).start();
+//            if (!mIsImageHidden) {
+//                mIsImageHidden = true;
+//
+//
+//            }
+        }
+
+        if (currentScrollPercentage < PERCENTAGE_TO_SHOW_IMAGE) {
+            ViewCompat.animate(mFab).scaleY(1).scaleX(1).start();
+//            if (mIsImageHidden) {
+//                mIsImageHidden = false;
+//
+//            }
+        }
+
+    }
 }
